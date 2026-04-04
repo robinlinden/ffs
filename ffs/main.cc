d20946b4de98b58a6317b7ed3d2231e8e7c14f7b
Robin Linden <dev@robinlinden.eu>
2026-04-05 00:57:54 +0200

    ffs: Initial work on query support
    For now, we only find the relevant build files.

--- ffs/main.cc
+++ ffs/main.cc
@@ -7,6 +7,7 @@
 #include "starlark/tokenizer.h"
 
 #include <cassert>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -27,6 +28,46 @@ std::string to_string(std::vector<starlark::Token> const &tokens) {
     }
 
     return std::move(ss).str();
+}
+
+// Find the first directory w/ a MODULE.bazel above a given path.
+std::optional<std::filesystem::path> project_root(std::filesystem::path from) {
+    assert(std::filesystem::is_directory(from));
+
+    do {
+        if (std::filesystem::exists(from / "MODULE.bazel")) {
+            return from;
+        }
+
+        from = from.parent_path();
+        std::cerr << from << std::endl;
+    } while (from != from.parent_path());
+
+    return std::nullopt;
+}
+
+std::optional<std::vector<std::filesystem::path>>
+build_files_from_pattern(std::filesystem::path const &from, std::string_view pattern) {
+    if (pattern != "...") {
+        // TODO(robinlinden): Support patterns.
+        std::cerr << "Unsupported pattern '" << pattern << "'.\n";
+        return std::nullopt;
+    }
+
+    auto root_path = project_root(from);
+    if (!root_path) {
+        std::cerr << "Unable to find ffs project root for folder '" << from << "'.\n";
+        return std::nullopt;
+    }
+
+    std::vector<std::filesystem::path> found;
+    for (auto const &dir : std::filesystem::recursive_directory_iterator{from}) {
+        if (std::filesystem::exists(dir.path() / "BUILD.bazel")) {
+            found.push_back(dir.path() / "BUILD.bazel");
+        }
+    }
+
+    return found;
 }
 
 int run_debug(int argc, char **argv) {
@@ -59,9 +100,31 @@ int run_debug(int argc, char **argv) {
     return 0;
 }
 
+int run_query(int argc, char **argv) {
+    assert(argc == 3);
+
+    auto cwd = std::filesystem::current_path();
+    auto build_files = build_files_from_pattern(cwd, argv[2]);
+    if (!build_files) {
+        std::cerr << "Failed to find build files.\n";
+        return 1;
+    }
+
+    // TODO(robinlinden): Get targets from build files.
+    for (auto const &build_file : *build_files) {
+        std::cout << build_file << '\n';
+    }
+
+    return 0;
+}
+
 } // namespace
 
 int main(int argc, char **argv) {
+    if (argc == 3 && argv[1] == std::string_view{"query"}) {
+        return run_query(argc, argv);
+    }
+
     if (argc != 2) {
         std::string_view name = argv[0] != nullptr ? argv[0] : "<bin>";
         std::cerr << "Usage: " << name << " <input_file>\n";
